9f704f7aca55d4cb9c7ddcee59d5c1bb38bb895b
Marco T.C. (BadWolf) <marco@badwolf.one>
2018-08-08 16:55:33 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/xiaomi/dipper/dipper-vendor.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/sirius/sirius-vendor.mk)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2248
+TARGET_SCREEN_HEIGHT := 2244
 TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
@@ -26,7 +26,7 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Inherit from sdm845-common
-$(call inherit-product, device/xiaomi/sdm845-common/sdm845.mk)
+$(call inherit-product, device/xiaomi/sdm710-common/sdm710.mk)
 
 # Input
 PRODUCT_COPY_FILES += \
